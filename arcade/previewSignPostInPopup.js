39dead301a4af3833b6be9e00e3ee2e2ceab621c
Inge Anundskås <inge.anundskas@gmail.com>
2023-04-18 16:13:17 +0200

    Subsign font size update

--- arcade/previewSignPostInPopup.js
+++ arcade/previewSignPostInPopup.js
@@ -17,8 +17,9 @@ function createIMG(signType, height) {
 function createSubSignHTML(f) {
   var imgURL = "https://ingean.github.io/kystverket-skiltregistrering/bilder/Underskilt_liten.jpg"
   var style = `font-weight:bold;
+             font-size: 10px;
              text-align: center;
-             padding: 3px 0px;
+             padding: 5px 0px;
              width:94px;height:28px;
              background-image: url(${imgURL});
              background-repeat: no-repeat;`
--- arcade/previewSubSignInPopup.js
+++ arcade/previewSubSignInPopup.js
@@ -1,7 +1,8 @@
 var imgURL = "https://ingean.github.io/kystverket-skiltregistrering/Underskilt_liten.jpg"
 var style = `font-weight:bold;
+             font-size: 10px;
              text-align: center;
-             padding: 3px 0px;
+             padding: 5px 0px;
              width:94px;height:28px;
              background-image: url(${imgURL});
              background-repeat: no-repeat;`
