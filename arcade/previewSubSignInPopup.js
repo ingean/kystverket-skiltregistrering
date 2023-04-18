1f478957ff38102492a133e2ec68fc1fdf5b15d1
Inge Anundskås <inge.anundskas@gmail.com>
2023-04-18 10:38:36 +0200

    Fixed text alignment

--- arcade/previewSubSignInPopup.js
+++ arcade/previewSubSignInPopup.js
@@ -1,6 +1,7 @@
 var imgURL = "https://ingean.github.io/kystverket-skiltregistrering/Underskilt_liten.jpg"
 var style = `font-weight:bold;
-             padding: 3px 10px 3px 10px;
+             text-align: center;
+             padding: 3px 0px;
              width:94px;height:28px;
              background-image: url(${imgURL});
              background-repeat: no-repeat;`
